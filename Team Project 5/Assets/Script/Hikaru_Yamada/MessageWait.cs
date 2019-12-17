39b7d8d38baa42c338dc525d8ceac2203068cef2
ChoeHwi <hwi26261@gmail.com>
2019-12-17 15:55:45 +0900

    bateban

---
+++ Team Project 5/Assets/Script/Hikaru_Yamada/MessageWait.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageWait : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("DeleteMs", 1f);
+    }
+
+    public void DeleteMs()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
