b18e9caa0f5b47d42cdfd12a7f6f5e8f6e87030b
unknown <you@example.com>
2020-01-25 10:37:09 +0900

    hint

---
+++ Team Project 5/Assets/text/hintsystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//この宣言が必要
+
+public class hintsystem : MonoBehaviour
+{
+    [SerializeField] Text buttonText;
+    [SerializeField] Text hintText;
+    [SerializeField] GameObject Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void hint()
+    {
+        Panel.SetActive(true);
+        switch (0)
+        {
+            case 0:
+                hintText.text = "なんもねーよ";
+                break;
+
+            default:
+        }
+    }
+}
