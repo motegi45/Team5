cca8b2c28806fad2f1ac8ef74f0c6205ffbcfa06
ChoeHwi <hwi26261@gmail.com>
2020-02-12 15:36:18 +0900

    move

---
+++ Team Project 5/Assets/Script/Hikaru_Yamada/Reset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneSaverCh.diary = false;
+        SceneSaverCh.penLight = false;
+        SceneSaverCh.key = false;
+        SceneSaverCh.doa = false;
+        SceneSaverCh.cookingCrea = false;
+        SceneSaverCh.enterCrea = false;
+        SceneSaverCh.stoneOn1 = false;
+        SceneSaverCh.stoneOn2 = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
